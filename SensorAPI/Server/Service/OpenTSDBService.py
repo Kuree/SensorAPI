02b6a50b720e4a88058813369db9da7dcbf9c9be
Kuree <xmlc001@gmail.com>
2014-06-10 16:33:35 -0400

    Add service

---
+++ SensorAPI/Server/Service/OpenTSDBService.py
@@ -0,0 +1,35 @@
+from spyne.decorator import srpc
+from spyne.service import ServiceBase
+from spyne.model.primitive import String
+import json
+
+class OpenTSDBService(ServiceBase):
+
+    @srpc(String, _returns=String)
+    def put(data):
+        jsonData = []
+        for d in data:
+            jsonData += [d]
+        url = 'http://{0}:{1}/api/put?details'.format("134.82.132.98", 4242)
+        return OpenTSDBService._putRequest(url, jsonData)
+
+    @srpc(String, _returns=String)
+    def query(data):
+        url = 'http://{0}:{1}/api/query'.format("134.82.132.98", 4242)
+        return OpenTSDBService._putRequest(url, data)
+
+    @srpc(String, _returns=String)
+    def queryLast(data):
+        url = 'http://{0}:{1}/api/query/last'.format("134.82.132.98", 4242)
+        return OpenTSDBService._putRequest(url, data)
+
+    @staticmethod
+    def _putRequest(url, data):
+        try:
+            print "receive", data
+            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+            r = requests.post(url, data=json.dumps(data), headers=headers)
+            return r.text
+        except Exception as e:
+            return e.message
+#
